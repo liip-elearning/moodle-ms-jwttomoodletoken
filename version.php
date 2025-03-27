993f8876f5d71849e3b8d112a94f9efb466e0641
Didier 'OdyX' Raboud <didier.raboud@liip.ch>
2025-03-27 08:53:25 +0100

    Bump version number

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2024082300;
+$plugin->version = 2025032700;
 $plugin->requires = 2019052000; // Moodle 3.7
 $plugin->component = 'local_jwttomoodletoken';
 
